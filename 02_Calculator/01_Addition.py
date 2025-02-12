b67e5cc45df99b2ecd6a7039091f085b16bca39c
Baranthean <m24153@hb.dhbw-stuttgart.de>
2025-02-12 16:20:09 +0000

    gh

---
+++ 02_Calculator/01_Addition.py
@@ -0,0 +1,9 @@
+print ("Geben Sie eine Zahl A ein:")
+stringA = input()
+stringB = input("Geben Sie eine Zahl B ein:")
+ergebnis = int(stringA) + int(stringB)
+print (ergebnis)
+Beenden=input("Zum Beenden betätigen Sie bitte die Entertaste")
+print (Beenden)
+              
+
---
+++ 02_Calculator/02_Division.py
@@ -0,0 +1,13 @@
+print ("Geben Sie eine Zahl A ein:")
+stringA = input()
+stringB = input("Geben Sie eine Zahl B ein:")
+if stringB == "0" : 
+    print ("Bitte geben Sie eine gültige Zahl ein")
+    stringB = input("Geben Sie eine Zahl B ein:")
+ergebnis = int(stringA) / int(stringB)
+print (ergebnis)
+Beenden=input("Zum Beenden betätigen Sie bitte die Entertaste")
+print (Beenden)
+              
+
+
