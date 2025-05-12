f5c705623ec7a40c426a1c1d8a89532c40b811a4
Baranthean <m24153@hb.dhbw-stuttgart.de>
2025-05-12 12:50:38 +0000

    1

--- 05_test.py/Aufgabe01.py
+++ 05_test.py/Aufgabe01.py
@@ -4,10 +4,19 @@ i=1
 while i <= 3:
     wurf = random.randint(1,6)
     print (wurf)
-    i = i+1
-    if wurf == 6 and i<=2:
+    if i==1 and wurf==6:
         wurf = random.randint(1,6)
         print (wurf)
-        i+1
+        i=i+2
+    if wurf != 6:
+           i=i+1
+    else:
+        if i==2 and wurf==6:
+         wurf = random.randint(1,6)
+         i=i+1
+        if wurf !=6:
+            i=i+1
+    if i==3 and wurf==6:
+        break
 if i>3:
     print("Keine Würfe mehr!")
